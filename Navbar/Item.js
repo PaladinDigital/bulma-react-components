c0282de55e8fb8c0cc09e8cbbb059aa2dcd74c43
davidcraig <davidcraig1986@gmail.com>
2019-01-16 12:16:24 +0000

    Add ClassName and allow child content other than a simple title to also be rendered (such as FA icons).

--- Navbar/Item.js
+++ Navbar/Item.js
@@ -3,8 +3,9 @@ import React from 'react'
 export class NavbarItem extends React.Component {
   render () {
     return (
-      <a href={this.props.url}>
-        {this.props.title}
+      <a className="navbar-item" href={this.props.url}>
+        {this.props.title || null}
+        {this.props.children || null}
       </a>
     )
   }
