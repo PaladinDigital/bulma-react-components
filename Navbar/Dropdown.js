e3f9eb4053c7f816c7626588355c9784d7325b92
davidcraig <david@davidcraig.eu>
2019-07-22 22:40:25 +0100

    Create dropdown nav item component.

--- Navbar/Dropdown.js
+++ Navbar/Dropdown.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
 export class DropdownMenu extends React.Component {
+  render () {
+    return (
+      <div className="navbar-item has-dropdown is-hoverable">
+        <a className="navbar-link">
+          {this.props.title}
+        </a>
 
+        <div className="navbar-dropdown">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
 }
